6192788d013f139404bf48496582353060fb9fcd
Z-bin <1134636325@qq.com>
2022-08-13 16:15:25 +0800

    feat: 更新翻译

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
 #include "mainwindow.h"
-#include "graphicsview.h"
 #include <QApplication>
 #include <QCommandLineParser>
-#include <QUrl>
+#include <QDir>
 #include <QTranslator>
+#include <QUrl>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     QTranslator translator;
-    translator.load(QString("PineapplePictures_%1").arg(QLocale::system().name()), ":/i18n/");
+    QString qmDir;
+    qmDir = QT_STRINGIFY(QM_FILE_INSTALL_DIR);
+    translator.load(QString("PineapplePictures_%1").arg(QLocale::system().name()), qmDir);
     a.installTranslator(&translator);
 
+    // parse commandline arguments
     QCommandLineParser parser;
-    parser.addPositionalArgument("File List", QCoreApplication::translate("main", "File list."));
-    parser.process(a);
+    parser.addPositionalArgument("File list", QCoreApplication::translate("main", "File list."));
     parser.addHelpOption();
+
+    parser.process(a);
 
     QStringList urlStrList = parser.positionalArguments();
     QList<QUrl> urlList;
-    for (const QString &str : urlStrList) {
+    for (const QString & str : urlStrList) {
         QUrl url = QUrl::fromLocalFile(str);
         if (url.isValid()) {
             urlList.append(url);
@@ -30,7 +35,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    if (!urlList.empty()) {
+    if (!urlList.isEmpty()) {
         w.showUrls(urlList);
         w.adjustWindowSizeBySceneRect();
     }
